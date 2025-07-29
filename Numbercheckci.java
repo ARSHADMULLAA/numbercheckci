74c9e08b83d97b7d4035844dfa99414f273f95a3
ARSHADMULLAA <arshadmulla874@gmail.com>
2025-07-29 10:30:27 +0530

    save

---
+++ Numbercheckci.java
@@ -0,0 +1,8 @@
+class Numbercheckci {
+    public static void main(String[] args) {
+        int  num = 10;
+        if (num > 0) {
+            System.out.println("Positive number");
+        }
+    }
+}
